b520ea8217fdbbdeb0c45be16c6701564e742b28
Hoang Nguyen <hoang21091989 [at] gmail.com>
2021-09-21 16:33:19 +0800

    Initial project setup

---
+++ HNNetworking/HNNetworking.h
@@ -0,0 +1,18 @@
+//
+//  HNNetworking.h
+//  HNNetworking
+//
+//  Created by Hoang Nguyen on 21/9/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HNNetworking.
+FOUNDATION_EXPORT double HNNetworkingVersionNumber;
+
+//! Project version string for HNNetworking.
+FOUNDATION_EXPORT const unsigned char HNNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HNNetworking/PublicHeader.h>
+
+
